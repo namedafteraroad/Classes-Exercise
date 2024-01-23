3ed570e41dafeafe581f1e00d901b7a007860e6e
will street <will@williamdstreet.com>
2024-01-23 13:34:17 -0600

    first commit, dot and ois

---
+++ Classes/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car   //Constructor
+    {
+
+        public string Make { get; set; }   //string property
+        public string Model { get; set; }  //string property
+        public int Year { get; set; }      //int property
+
+    }
+
+    
+
+
+
+
+
+
+
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //dot notation
+            Car Pony = new Car();
+            Pony.Make = "Ford";
+            Pony.Model = "Bronco";
+            Pony.Year = 1966;
+
+            //OIS
+            Car car2 = new Car() {Make = "Ford", Model = "Bronco II", Year = 1983};
+
+           
+
         }
     }
 }
